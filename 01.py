2a39851cc66d48a481b7dd02b5d652e8b42be48b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-12-01 21:03:27 +0100

    1/done - wrap around/minor

--- 01.py
+++ 01.py
@@ -1,24 +1,19 @@
-D = open(0).read().strip().splitlines()
-
-r1,r2 = 0,0
-i = 50
-j = i
-for line in D:
-    d, n = line[0], int(line[1:])
+D = [(_[0], int(_[1:]))for _ in open(0).read().splitlines()]
+r1,i=0,50
+r2,j=0,i
+for d,n in D:
     lr = 1
     if d == 'L':
         lr = -1
-    i += n*lr
+    i += lr*n
     i %= 100
     r1 += (i==0)
-
     # p2
     for _ in range(n):
         j += lr
         j %= 100
         r2 += (j==0)
-print('r1/',r1)
-print('r2/',r2)
-
+print('res1/',r1)
+print('res2/',r2)
 assert r1 in (3,1123)
 assert r2 in (6,6695)
