0575f6ab3e4c7b64c0feda5c87020fc3360f86bf
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-12-02 18:18:40 +0100

    2/done - repeated substr /added p2

--- 02.py
+++ 02.py
@@ -1,4 +1,5 @@
 r1,r2 = 0,0
+r3 = 0
 for a in [_.split('-') for _ in open(0).read().strip().split(',')]:
     l,r = [int(_)for _ in a]
     for n in range(l,r+1):
@@ -9,6 +10,14 @@ for a in [_.split('-') for _ in open(0).read().strip().split(',')]:
         # p2
         if s in (s+s)[1:-1]:
             r2 += n
+        # p2 way 2
+        for sub in range(1,L//2+1):
+            if L % sub != 0:
+                continue
+            if s==s[:sub]*(L//sub):
+                r3 += n
+                break
 print(r1,r2)
 assert r1 in (1227775554,55916882972)
 assert r2 in (4174379265,76169125915)
+assert r2 == r3
