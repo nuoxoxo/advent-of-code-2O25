d5cc6acfe6de2d3475fefbdf7e3b3e258f5c9af7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-12-02 09:31:21 +0100

    2/done - repeated substr / minor

--- 02.py
+++ 02.py
@@ -7,12 +7,8 @@ for a in [_.split('-') for _ in open(0).read().strip().split(',')]:
         if L%2==0 and s[:L//2]==s[L//2:]:
             r1 += n
         # p2
-        ls = s+s
-        ls = ls[1:L*2-1]
-        #print(s,ls)
-        if s in ls:
+        if s in (s+s)[1:-1]:
             r2 += n
-            #break
 print(r1,r2)
 assert r1 in (1227775554,55916882972)
 assert r2 in (4174379265,76169125915)
