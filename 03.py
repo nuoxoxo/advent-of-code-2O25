b36eb9f9ea8bf69a18af936d89d94b3b5e0dda89
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-12-03 08:10:40 +0100

    3/greedy mono stack / simplified

--- 03.py
+++ 03.py
@@ -5,17 +5,18 @@ for a in open(0).read().splitlines():
     for l in range(N-1):
         for r in range(l+1,N):
             nums.append(int(a[l]+a[r]))
-    #print(max(nums))
     r1+=max(nums)
     # p2
     Q = []
-    for i,c in enumerate(a):
-        while Q and int(Q[-1]) < int(c) and N-i + len(Q)-1 >= 12:
+    canuse = len(a)-12
+    for c in a:
+        while Q and canuse > 0 and int(Q[-1]) < int(c):
+            canuse -= 1
             Q.pop()
         Q.append(c)
     while len(Q) > 12:
         Q.pop()
-    print(''.join(Q))
     r2+=int(''.join(Q))
 print(r1,r2)
 assert(r2 in (170147128753455,3121910778619))
+assert(r1 in (17092,357))
