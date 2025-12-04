533308e18ae25659e0dbfb1ccda97041865dceb0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-12-04 22:04:59 +0100

    4/done - grid check - minor

--- 04.py
+++ 04.py
@@ -1,5 +1,4 @@
 G = open(0).read().splitlines()
-#R,C = len(G),len(G[0])
 R=len(G)
 C=R
 S=set()
@@ -10,7 +9,6 @@ def ok2(r,c) -> bool:
         rr,cc=r+dr,c+dc
         if -1<rr<R and -1<cc<C and (rr,cc) not in S and G[rr][cc]=='@':
             res += 1
-    #print(r,c,res)
     return res < 4
 def ok(r,c) -> bool:
     res = 0
@@ -19,19 +17,17 @@ def ok(r,c) -> bool:
         rr,cc=r+dr,c+dc
         if -1<rr<R and -1<cc<C and G[rr][cc]=='@':
             res += 1
-    #print(r,c,res)
     return res < 4
 r1 = 0    
 for r in range(R):
     for c in range(C):
         if G[r][c] == '@' and ok(r,c):
-            #print(r,c)
             r1 += 1
 yes = True
 count = 0
 while yes:
     count += 1
-    print(count)
+    print('it/',count)
     yes = False
     for r in range(R):
         for c in range(C):
@@ -39,4 +35,3 @@ while yes:
                 S.add((r,c))
                 yes = True
 print(r1,len(S))
-print(S)
